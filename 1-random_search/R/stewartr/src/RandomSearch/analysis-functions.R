ac8fb7658307b19f49ff987360bc1a59fab6f02a
Stewart, Robert <Robert.Stewart@espn.com>
2017-01-21 22:14:30 -0500

    Got it working.

--- 1-random_search/R/stewartr/src/RandomSearch/analysis-functions.R
+++ 1-random_search/R/stewartr/src/RandomSearch/analysis-functions.R
@@ -1,24 +1,14 @@
 rm(list = ls())
 
 iterations <- 20
-# runif([howManyNumbers,start,end(non-inclusive)])
-number.list <- c(runif(100, 0, 101))
-number.list
+number.list <- c(runif(100, 1, 101))
+sample.list <- sample(number.list, iterations)
 
-#lowest <- runif(1, min<-1, max<-length(number.list))
-number.iterations <- 0
-random.search <- function () {
-  get.rand.in.set <- function () runif(1, 1, length(number.list))
-  lowest <- get.rand.in.set()
-  for (i in iterations-1) {
-    #rand.in.set = get.rand.in.set()
-    lowest <- min(c(lowest, rand.in.set))
-    #number.iterations.values <<- number.iterations.values + rand.in.set
-  }
+random.search <- function (random.selections) {
+  lowest <- random.selections[1]
+  for (i in 2:length(random.selections)) lowest <- min(c(lowest, random.selections[i]))
   return(lowest)
-}
+} 
+random.search(sample.list)
 
-#random.search()
-
-#lowest
 
