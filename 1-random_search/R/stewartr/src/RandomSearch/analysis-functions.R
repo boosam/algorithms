0e074eba1a8ad6d64988f7f502472d989f742ef3
Stewart, Robert <Robert.Stewart@espn.com>
2017-01-21 22:21:13 -0500

    Knocked it down to 10 lines (and removed for loop- apparently those are notoriously slow in R)

--- 1-random_search/R/stewartr/src/RandomSearch/analysis-functions.R
+++ 1-random_search/R/stewartr/src/RandomSearch/analysis-functions.R
@@ -5,9 +5,7 @@ number.list <- c(runif(100, 1, 101))
 sample.list <- sample(number.list, iterations)
 
 random.search <- function (random.selections) {
-  lowest <- random.selections[1]
-  for (i in 2:length(random.selections)) lowest <- min(c(lowest, random.selections[i]))
-  return(lowest)
+  return(min(random.selections))
 } 
 random.search(sample.list)
 
