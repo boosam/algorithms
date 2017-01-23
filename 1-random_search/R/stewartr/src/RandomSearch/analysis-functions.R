8be2434f3e4f1ea733581cd4b9245c3138699ed9
Stewart, Robert <Robert.Stewart@espn.com>
2017-01-23 17:48:10 -0500

    Added a graph of the problem. Broke out magic numbers into variables Renamed a couple of items for consistency Added comments to all lines Added squaring of data set (was just comparing numbers without squaring)

--- 1-random_search/R/stewartr/src/RandomSearch/analysis-functions.R
+++ 1-random_search/R/stewartr/src/RandomSearch/analysis-functions.R
@@ -1,12 +1,23 @@
-rm(list = ls())
+rm(list = ls()) # reset R environment
 
-iterations <- 20
-number.list <- c(runif(100, 1, 101))
-sample.list <- sample(number.list, iterations)
+sample.size <- 20 # iterations
+number.count <- 100 # objects in total search space
+number.range.min <- -10 # min input number
+number.range.max <- 10 # max input number
+number.func <- function(input) input^2 # treat this as f(x)
+number.list <- number.func(c(runif(number.count, number.range.min, number.range.max))) # gets random numbers and applies f(x)
+sample.list <- sample(number.list, sample.size) # get a set of random data to search in
 
 random.search <- function (random.selections) {
-  return(min(random.selections))
+  min(random.selections) #return lowest value
 } 
-random.search(sample.list)
+lowest <- random.search(sample.list) # store the lowest value globally (to graph
 
-
+graph.results <- function() {
+  dev.new() # open new graph window
+  par(usr=c(0, sample.size, number.range.min, number.range.max)) #graph parameters
+  plot(number.list, type="p") # graph the full number set in black
+  points(match(sample.list, number.list), sample.list, col="red") # graph the whole sample set in red 
+  points(which(number.list == lowest),lowest, col="green") # graph the best-fit from the sample in green
+}
+graph.results() # build the graph
