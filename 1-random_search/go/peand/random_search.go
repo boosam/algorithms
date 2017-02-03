90fb46965b0b15b57d193eddcd6bae6ec032a548
Pean <david.pean@gmail.com>
2017-02-02 20:55:33 -0500

    closest city

--- 1-random_search/go/peand/random_search.go
+++ 1-random_search/go/peand/random_search.go
@@ -1,54 +1,92 @@
 package main
+
 import (
-	"fmt"
-	"math/rand"
+	"log"
 	"math"
+	"strconv"
 	"time"
+	"math/rand"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
+
 func main() {
+	var targetLatitude float64 = 41.6718;
+	var targetLongitude float64 = 72.9493;
+	var NumIterations int = 500;
+	var bestItem Item;
+	var bestDistance float64;
 
-	var NumIterations int = 100
-	var problemSpace int = 2
-	var best float64
-	var searchSpace [2][2]int
-
-
-	for i := 0; i < problemSpace; i++ {
-		searchSpace[i] = [...]int{-5, 5}
-	}
-
+	svc := dynamodb.New(session.New(&aws.Config{Region: aws.String("us-east-1")}))
+	
 	for i := 0; i < NumIterations; i++ {
+		rand.Seed(time.Now().Unix())
+		randomId := rand.Intn(2895921)
 
-		candidate := randomSolution(searchSpace)
-		//fmt.Println("candidate:", candidate)
-		value := randomCalculation(candidate)
-		fmt.Println("Value:", value)
-		if i == 0 || value < best{
-			best = value
-		}
+		params := &dynamodb.GetItemInput{
+	        Key: map[string]*dynamodb.AttributeValue{
+	            "ID": {
+	                N: aws.String(strconv.Itoa(randomId)),
+	            },
+	        },
+	        TableName:            aws.String("WorldCities"),
+	    }
 
+	    resp, err := svc.GetItem(params)
+	  	oneItem := Item{}
+	  	err = dynamodbattribute.UnmarshalMap(resp.Item, &oneItem)
+
+	  	longFloat, err := strconv.ParseFloat(oneItem.Longitude, 64);
+	  	latFloat, err := strconv.ParseFloat(oneItem.Latitude, 64);
+	  	distance := Distance(targetLatitude, targetLongitude, latFloat, longFloat)
+	    if err == nil {
+	    	//log.Println( oneItem.Latitude )
+	    	//log.Println( oneItem.Longitude )
+	    	if i == 0 || distance < bestDistance{
+				bestItem = oneItem
+				bestDistance = distance
+			}
+
+	    	//log.Println( distance )
+	    }
 	}
-	fmt.Println("Best:", best)
-
+	log.Println("Closest city:")
+	log.Println( bestItem.CityName )
+	log.Println( bestItem.CountryCode )
+	log.Println( bestDistance )
+   	
 }
 
-func randomSolution(searchSpace [2][2]int)[2]float64{
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1) 
-
-	var solution [2]float64
-	for i := 0; i < 2; i++ {
-		solution[i] = float64(searchSpace[i][0]) + (float64( searchSpace[i][1] - searchSpace[i][0] ) * r1.Float64() )
-	}
-	return solution
-
+type Item struct {
+    ID     int  
+    ASCIICityName        string    `dynamo:"ASCIICityName"`
+    CityName  string    `dynamo:"CityName"`
+    CountryCode   string    `dynamo:"CountryCode"`
+    Latitude string    `dynamo:"Latitude"`
+    Longitude string    `dynamo:"Longitude"`
+    Region string    `dynamo:"Region"`
 }
 
-func randomCalculation(candidate [2]float64)float64{
-	calc := 0.0
-	for i := 0; i < 2; i++ {
-		calc = calc + math.Pow(candidate[i],2) 
-	}
-	return calc
+
+func hsin(theta float64) float64 {
+	return math.Pow(math.Sin(theta/2), 2)
 }
 
+func Distance(lat1, lon1, lat2, lon2 float64) float64 {
+	// convert to radians
+  // must cast radius as float to multiply later
+	var la1, lo1, la2, lo2, r float64
+	la1 = lat1 * math.Pi / 180
+	lo1 = lon1 * math.Pi / 180
+	la2 = lat2 * math.Pi / 180
+	lo2 = lon2 * math.Pi / 180
+
+	r = 6378100 // Earth radius in METERS
+
+	// calculate
+	h := hsin(la2-la1) + math.Cos(la1)*math.Cos(la2)*hsin(lo2-lo1)
+
+	return 2 * r * math.Asin(math.Sqrt(h))
+}
